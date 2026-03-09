e40ca2bbe0abd65f98daf147d0a52ddcad13698d
yushau <akandeoluwanifemi@gmail.com>
2026-03-09 17:02:08 +0000

    fix issue with app not connecting to redis due to container name and also added a retry login to connect to redis

--- app/src/index.js
+++ app/src/index.js
@@ -8,7 +8,21 @@ const client = redis.createClient({
   url: process.env.REDIS_URL || "redis://redis:6379"
 });
 
-client.connect();
+// Retry until Redis is ready
+async function connectRedis() {
+  while (true) {
+    try {
+      await client.connect();
+      console.log("Connected to Redis");
+      break;
+    } catch (err) {
+      console.log("Redis not ready, retrying...");
+      await new Promise(res => setTimeout(res, 1000));
+    }
+  }
+}
+
+connectRedis();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
@@ -22,17 +36,14 @@ app.get("/status", (req, res) => {
 });
 
 app.post("/process", async (req, res) => {
-
   const data = req.body;
-
   await client.set("last_payload", JSON.stringify(data));
-
   res.json({
     message: "Payload processed",
     data
   });
 });
 
-app.listen(3000, () => {
+app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
