33779f251734296de4d110298580ca83f885cb5d
yushau <akandeoluwanifemi@gmail.com>
2026-03-11 11:38:27 +0000

    updated the changes in the backend

--- terraform/modules/app-infra/main.tf
+++ terraform/modules/app-infra/main.tf
@@ -313,16 +313,3 @@ resource "aws_lb_target_group_attachment" "app" {
   port             = var.app_port
 }
 
-############################
-# OUTPUTS
-############################
-
-output "app_public_ip" {
-  value       = aws_eip.app.public_ip
-  description = "Elastic IP of the EC2 app server"
-}
-
-output "alb_dns_name" {
-  value       = aws_lb.app.dns_name
-  description = "DNS name of the Application Load Balancer"
-}
--- terraform/modules/app-infra/output.tf
+++ terraform/modules/app-infra/output.tf
@@ -2,9 +2,9 @@
 # OUTPUTS
 ############################
 
-output "app_instance_id" {
-  value       = aws_instance.app.id
-  description = "EC2 instance ID for SSM access"
+output "app_public_ip" {
+  value       = aws_eip.app.public_ip
+  description = "Elastic IP of the EC2 app server"
 }
 
 output "alb_dns_name" {
