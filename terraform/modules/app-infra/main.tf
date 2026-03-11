cbe18ed0f192eff4b7969869a95df3616cc7f3d3
yushau <akandeoluwanifemi@gmail.com>
2026-03-11 08:57:30 +0000

    added details

--- terraform/modules/app-infra/main.tf
+++ terraform/modules/app-infra/main.tf
@@ -170,6 +170,34 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 ############################
+# IAM ROLE FOR SSM
+############################
+
+resource "aws_iam_role" "ec2_ssm_role" {
+  name = "${var.env}-ec2-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy" {
+  role       = aws_iam_role.ec2_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "${var.env}-ec2-ssm-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
+
+
+############################
 # EC2 INSTANCE
 ############################
 
--- terraform/modules/app-infra/output.tf
+++ terraform/modules/app-infra/output.tf
@@ -1,1 +1,13 @@
-#place_holder
+############################
+# OUTPUTS
+############################
+
+output "app_instance_id" {
+  value       = aws_instance.app.id
+  description = "EC2 instance ID for SSM access"
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.app.dns_name
+  description = "DNS name of the Application Load Balancer"
+}
