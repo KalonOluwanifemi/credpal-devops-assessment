6d8d575c48788deec1745ea0efeab94adbe922a8
yushau <akandeoluwanifemi@gmail.com>
2026-03-11 01:12:00 +0000

    removed the listener

--- terraform/modules/app-infra/main.tf
+++ terraform/modules/app-infra/main.tf
@@ -250,17 +250,4 @@ resource "aws_lb_target_group_attachment" "app" {
   port             = var.app_port
 }
 
-############################
-# HTTP LISTENER
-############################
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
